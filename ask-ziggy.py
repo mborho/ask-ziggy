bcb275d05dbf65a10d1dfa776a6be89c275d03a3
martin <martin@dude.(none)>
2010-01-15 17:30:30 +0100

    added gpl notice

--- ask-ziggy.py
+++ ask-ziggy.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
-# Copyright 2010 Martin Borho <martin@borho.net>
-# GPL - see License.txt for details
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation; version 3 only.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+# Copyright 2010 Martin Borho <martin@borho.net>
 import sys
 import gtk
 import osso
